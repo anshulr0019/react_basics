7209d1065d4be0bda5edbe4b79bc78256bb24750
Anshul Kumar <anshulrajput338@gmail.com>
2025-01-15 20:50:51 +0530

    i am just learning the basics, first lecture of react is done

--- src/App.js
+++ src/App.js
@@ -1,11 +1,49 @@
 import logo from './logo.svg';
 import './App.css';
-
+import Item from './components/items'
+import ItemDate from './components/ItemDate'; 
+import Card from './components/Card';
 function App() {
+  const response = [
+    {
+    itemName: "Nirma",
+    itemDate: "20",
+    itemMonth: "June",
+    itemYear: "1998"
+  },
+  {
+    itemName: "Nirma2",
+    itemDate: "202",
+    itemMonth: "June2",
+    itemYear: "19982"
+  },
+  {
+    itemName: "Nirma3",
+    itemDate: "203",
+    itemMonth: "June3",
+    itemYear: "19983"
+  }
+  ]
+  const itemTwoName = "SurfExcel"
   return (
-    <div className="App">
-      Hello jee
-    </div>
+    <Card>
+      <div>
+      <Item name = {response[0].itemName}>
+        Hello bachi
+        </Item> 
+      <ItemDate day = {response[0].itemDate} month={response[0].itemMonth}
+        year = {response[0].itemYear}></ItemDate>
+
+      <Item name = {response[1].itemName}></Item> 
+      <ItemDate day = {response[1].itemDate} month={response[1].itemMonth}
+        year = {response[1].itemYear}></ItemDate>
+
+      <Item name = {response[2].itemName}></Item> 
+      <ItemDate day = {response[2].itemDate} month={response[2].itemMonth}
+        year = {response[2].itemYear}></ItemDate>
+        <div className="App">Hello Jee</div>
+      </div>
+    </Card>
   );
 }
 
