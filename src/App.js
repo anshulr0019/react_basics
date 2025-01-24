b7169c6179c2c995943edb58ee5f3eb6347e3502
Anshul Kumar <anshulrajput338@gmail.com>
2025-01-24 11:02:28 +0530

    final commit for this minor project

--- src/App.js
+++ src/App.js
@@ -1,49 +1,45 @@
-import logo from './logo.svg';
-import './App.css';
-import Item from './components/items'
-import ItemDate from './components/ItemDate'; 
-import Card from './components/Card';
-function App() {
-  const response = [
+import React from 'react';
+import NewProduct from './components/NewProduct';
+
+import Products from './components/Products';
+
+const App = () => {
+  const products = [
     {
-    itemName: "Nirma",
-    itemDate: "20",
-    itemMonth: "June",
-    itemYear: "1998"
-  },
-  {
-    itemName: "Nirma2",
-    itemDate: "202",
-    itemMonth: "June2",
-    itemYear: "19982"
-  },
-  {
-    itemName: "Nirma3",
-    itemDate: "203",
-    itemMonth: "June3",
-    itemYear: "19983"
+      id: 'p1',
+      title: 'Nirma',
+      amount: 100,
+      date: new Date(2021, 8, 10),
+    },
+    { 
+      id: 'p2', 
+      title: 'Surf Excel', 
+      amount: 200, 
+      date: new Date(2021, 2, 2) },
+    {
+      id: 'p3',
+      title: 'Tide',
+      amount: 130,
+      date: new Date(2021, 12, 28),
+    },
+    {
+      id: 'p4',
+      title: 'Maggi',
+      amount: 450,
+      date: new Date(2021, 5, 5),
+    },
+  ];
+
+  function printProductData(data) {
+    console.log("i am inside APP.js")
+    console.log(data)
   }
-  ]
-  const itemTwoName = "SurfExcel"
+
   return (
-    <Card>
-      <div>
-      <Item name = {response[0].itemName}>
-        Hello bachi
-        </Item> 
-      <ItemDate day = {response[0].itemDate} month={response[0].itemMonth}
-        year = {response[0].itemYear}></ItemDate>
-
-      <Item name = {response[1].itemName}></Item> 
-      <ItemDate day = {response[1].itemDate} month={response[1].itemMonth}
-        year = {response[1].itemYear}></ItemDate>
-
-      <Item name = {response[2].itemName}></Item> 
-      <ItemDate day = {response[2].itemDate} month={response[2].itemMonth}
-        year = {response[2].itemYear}></ItemDate>
-        <div className="App">Hello Jee</div>
-      </div>
-    </Card>
+    <div>
+      <NewProduct/>
+      <Products items={products} />
+    </div>
   );
 }
 
